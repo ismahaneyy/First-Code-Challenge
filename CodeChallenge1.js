e1396b5037658a11df91f21c558709f792c27ad0
ismahaneyy <liciousmayrah@gmail.com>
2022-11-28 15:53:30 +0300

    added CodeChallenge1.js file which inputs student marks and outputs the grade

--- CodeChallenge1.js
+++ CodeChallenge1.js
@@ -1,6 +1,6 @@
 //enter prompts to input student marks
 let marks = prompt("Input Student Marks");
-//using if else statement to grade students according to their marks.  
+//using if else statement to grade students according to ones marks. 
 if (marks > 79 && marks <= 100) {
     console.log("Grade:A");
   } else if (marks >= 60 && marks <= 79) {
@@ -11,5 +11,7 @@ if (marks > 79 && marks <= 100) {
     console.log("Grade:D");
   } else if (marks>=0 && marks <40) {
     console.log("Grade:E");
+  }else {
+    console.log("enter valid marks");
   }
 
