6f9c317f127580858c57a4075cbfcd75543c60e7
ismahaneyy <liciousmayrah@gmail.com>
2022-11-28 11:00:44 +0300

    added CodeChallenge2.js which inputs speed of a car and outputs Ok if speed is normal.When speed exceeds speed limit output is demerit points and if demerit points exceed 12 output is License suspended

---
+++ CodeChallenge2.js
@@ -0,0 +1,14 @@
+//input speed of Car and demerit Points of a Car
+let speedOfCar = 90;
+let demeritPointsOfCar = (speedOfCar-70)/5;
+//using if statement to print the speedOfCar for speed of car is less than 70, the output is "OK".
+if(speedOfCar<70){
+    console.log("OK");
+}
+//for every 5km above speed limit(70), we output demerit points and if those demerit points exceed 12 we output License suspended.
+else{
+  console.log("demeritPoints:" + demeritPointsOfCar);
+  if(demeritPointsOfCar>12){
+    console.log("License suspended");
+  }
+}
