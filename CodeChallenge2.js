7541c68f2ae4597c0432560e3879432db788d413
ismahaneyy <liciousmayrah@gmail.com>
2022-11-30 19:24:42 +0300

    input speed of car and output Ok if speed is below 70, if above, we print points and if those points exceed 12 , we output License suspended

--- CodeChallenge2.js
+++ CodeChallenge2.js
@@ -2,11 +2,11 @@
 function speedOfCar(){
   let speedOfCar = 85;
 let demeritPointsOfCar = (speedOfCar-70)/5;
-//using if statement to print the speedOfCar for speed of car is less than 70, the output is "OK".
+//using if statement to Print the speedOfCar for speed of car is less than 70, the output is "OK".
 if(speedOfCar<70){
     console.log("OK");
 }
-//for every 5km above speed limit(70), we output demerit points and if those demerit points exceed 12 we output License suspended.
+//for every 5km above speed limit(70), We output demerit points and if those demerit points exceed 12 we output License suspended.
 else{
   console.log("demeritPoints:" + demeritPointsOfCar);
   if(demeritPointsOfCar>12){
