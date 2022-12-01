79ca9eb4a4d276e6f5c3f5461ad2004ce5fd2a2d
ismahaneyy <liciousmayrah@gmail.com>
2022-12-01 15:09:02 +0300

    added CodeChallenge1.js where you input student marks and it outputs the correct grade

--- CodeChallenge1.js
+++ CodeChallenge1.js
@@ -4,11 +4,11 @@ let marks = prompt("Input Student Marks");
 //using if else statement to grade students according to ones marks.
 if (marks > 79 && marks <= 100) {
     console.log("Grade:A");
-  } else if (marks >= 60 && marks <= 79) {
+  } else if (marks >= 60 && marks <=79) {
     console.log("Grade:B");
-   } else if (marks >=59  && marks <=69){
+   } else if (marks >=49  && marks <=59){
     console.log("Grade:C");
-  } else if (marks >= 40 && marks <= 49) {
+  } else if (marks >=40 && marks <49) {
     console.log("Grade:D");
   } else if (marks>=0 && marks <40) {
     console.log("Grade:E");
